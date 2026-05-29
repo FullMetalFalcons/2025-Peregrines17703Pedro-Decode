a788c2be9e02821a5cfda2a1143aa3eae12d1e12
Ethan Arcari <ethanarcari@gmail.com>
2026-05-29 14:21:05 -0400

    forgot this

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PeregrinesAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PeregrinesAuto.java
@@ -31,7 +31,7 @@ public class PeregrinesAuto extends OpMode {
     ElapsedTime autoTimer = new ElapsedTime();
     ElapsedTime ballTimer = new ElapsedTime();
     double delaySeconds = 0.0;
-    public static Pose endPose = new Pose();
+    public static Pose endPose = PeregrinesPos.startPos;
     final double AUTO_LENGTH_SECONDS = 30.0;
     final double AUTO_END_BUFFER_SECONDS = 1.0;
 
